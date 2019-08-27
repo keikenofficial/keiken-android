d15144f47bdc2235bc568af01e9ba321788f6ae6
Salvatore Matrisciano <matrisch@hotmail.it>
2019-08-27 15:17:46 +0200

    splashScreen e icona

---
+++ app/src/main/java/com/keiken/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.keiken;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Start home activity
+        startActivity(new Intent(SplashActivity.this, LauncherActivity.class));
+        // close splash activity
+        finish();
+    }
+}
+
+
