09516b06e18c0221815a37f49e4dcc8fbf8279d5
bigimattia <mattia.bigi@outlook.it>
2019-09-03 19:42:58 +0200

    Primo commit
    1) creati :
        a) createExperienceActivity (empty per ora)
        b) activity_create_experience (da finire!!)
    2) sistemati i vari parametri possibili alla registrazione/modifica del profilo in modo che la dimensione massima della string rispettasse le decisioni prese precedentemente.
    3)aggiunti a strings.xml le stringhe da usare in activity_create_experience

---
+++ app/src/main/java/com/keiken/view/activity/CreateExperienceActivity.java
@@ -0,0 +1,16 @@
+package com.keiken.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.keiken.R;
+
+public class CreateExperienceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_experience);
+    }
+}
