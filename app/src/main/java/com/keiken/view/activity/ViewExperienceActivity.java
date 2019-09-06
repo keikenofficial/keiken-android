70cce35130eeb6b54d7023a7f90bcf32dbddef33
Salvatore Matrisciano <matrisch@hotmail.it>
2019-09-07 00:27:00 +0200

    Update ViewExperienceActivity.java

--- app/src/main/java/com/keiken/view/activity/ViewExperienceActivity.java
+++ app/src/main/java/com/keiken/view/activity/ViewExperienceActivity.java
@@ -97,7 +97,7 @@ public class ViewExperienceActivity extends AppCompatActivity {
         TextView dateTV = findViewById(R.id.date);
         for(int i = 0; i<dateList.size(); i++){
             String tempDate = dateList.get(i).get(Calendar.DAY_OF_MONTH) + "/" + dateList.get(i).get(Calendar.MONTH) + "/" + dateList.get(i).get(Calendar.YEAR);
-            dateTV.setText(dateTV.getText() + tempDate + " (Posti rimasti: "+dateMap.get(dateList.get(i)).toString() + ")\n");
+            dateTV.setText(dateTV.getText() + tempDate + " (Posti disponibili: "+dateMap.get(dateList.get(i)).toString() + ")\n");
         }
 
 
