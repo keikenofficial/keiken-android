f244f1075d99906c9e431c358c04ac81b4ddf704
bigimattia <mattia.bigi@outlook.it>
2019-09-07 15:36:35 +0200

    Fix USER PHOTO FROM FB
    wasn't displaying during login

--- app/src/main/java/com/keiken/controller/ImageController.java
+++ app/src/main/java/com/keiken/controller/ImageController.java
@@ -248,22 +248,7 @@ public class ImageController {
     public static void setProfilePic(ImageView photo) {
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-        boolean externalProvider = false;
-        if (user != null && user.getPhotoUrl() != null) {
-            for (UserInfo info : user.getProviderData()) {
-                if (info.getProviderId().equals("facebook.com")) {
-                    new DownloadImageFromInternet(photo).execute(user.getPhotoUrl().toString() + "?type=large");
-                    externalProvider = true;
-                }
-                if (info.getProviderId().equals("google.com")) {
-                    new DownloadImageFromInternet(photo).execute(user.getPhotoUrl().toString() + "?sz=1080");
-                    externalProvider = true;
-                }
-            }
-            if (!externalProvider)
-                new DownloadImageFromInternet(photo).execute(user.getPhotoUrl().toString());
-        }
+        new DownloadImageFromInternet(photo).execute(user.getPhotoUrl().toString());
     }
 
     public static void setProfilePic(ImageView photo, int size) {
